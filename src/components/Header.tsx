d36fa29a51471eb1a51950514e6923138cc46c82
Galaceon <antoniogarcialtr@gmail.com>
2026-04-30 19:52:49 +0200

    Adding navegation between pages with Link reactRouter

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,4 @@
+import { Link, NavLink } from "react-router-dom"
 
 export default function Header() {
     return (
@@ -8,8 +9,9 @@ export default function Header() {
                         <img className="w-32" src="/public/logo.svg" alt="logotipo" />
                     </div>
 
-                    <nav>
-
+                    <nav className="flex gap-6">
+                        <Link to="/" className="text-white uppercase font-bold">Inicio</Link>
+                        <Link to="/favoritos" className="text-white uppercase font-bold">Favoritos</Link>
                     </nav>
                 </div>
             </div>
