95557ea36f4972de1251d58425bd50e94e35a5b3
Galaceon <antoniogarcialtr@gmail.com>
2026-05-01 18:28:02 +0200

    tailwind theming to add a bg image

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,7 +10,7 @@ export default function Header() {
     console.log(isHome)
 
     return (
-        <header className="bg-slate-800">
+        <header className={isHome ? "bg-[url('./public/bg.jpg')] bg-cover bg-center" : 'bg-slate-800'}>
             <div className="mx-auto container px-5 py-16">
                 <div className="flex justify-between items-center">
                     <div>
