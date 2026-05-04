8cf482c6e25e56b659b495dde0c2798156858e41
Galaceon <antoniogarcialtr@gmail.com>
2026-05-04 19:42:54 +0200

    finishing code to display drinks with styles

--- src/components/DrinkCard.tsx
+++ src/components/DrinkCard.tsx
@@ -7,9 +7,19 @@ type DrinkCardProps = {
 
 export default function DrinkCard({drink} : DrinkCardProps) {
     return (
-        <div>
+        <div className="shadow-xl rounded-2xl overflow-hidden">
+            <div className="overflow-hidden">
+                <img className="hover:scale-115 transition-transform hover:rotate-2" src={drink.strDrinkThumb} alt={`Imagen de ${drink.strDrink}`}></img>
+            </div>
 
-            <h2>{drink.strDrink}</h2>
+            <div className="p-7">
+                <h2 className="text-2xl truncate font-black">{drink.strDrink}</h2>
+                <button
+                    className="bg-orange-400 hover:bg-orange-500 mt-5 w-full p-3 font-bold text-white text-lg"
+                >
+                    Ver Receta
+                </button>
+            </div>
         </div>
     )
 }
