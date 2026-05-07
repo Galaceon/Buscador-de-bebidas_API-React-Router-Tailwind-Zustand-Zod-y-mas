1754bb6d2ea7300df951613d201e70035190184f
Galaceon <antoniogarcialtr@gmail.com>
2026-05-07 06:27:27 +0200

    creating slice to favorites

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -87,7 +87,8 @@ export default function Modal() {
 
                                         <button
                                             type='button'
-                                            onClick={}
+                                            // TODO
+                                            //onClick={}
                                             className='w-full rounded bg-orange-600 p-3 font-bold uppercase text-white shadow hover:bg-orange-500'
                                         >
                                             Agregar a Favoritos
